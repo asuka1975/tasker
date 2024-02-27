45e34145fb095e16b3d6791f27f1752af4e8ec47
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2024-02-28 00:00:21 +0900

    feat: add icons for limitAt

---
+++ frontend/task-frontend/src/app/_components/atom/LimitOverIcon.tsx
@@ -0,0 +1,11 @@
+type Props = {
+
+};
+
+export default function LimitOverIcon({}: Props) {
+    return (
+        <div className="flex justify-center items-center bg-red-500 rounded-sm w-4 h-4">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="white"><path d="M480-120q-33 0-56.5-23.5T400-200q0-33 23.5-56.5T480-280q33 0 56.5 23.5T560-200q0 33-23.5 56.5T480-120Zm-80-240v-480h160v480H400Z"/></svg>
+        </div>
+    )
+}
--- frontend/task-frontend/src/app/page.tsx
+++ frontend/task-frontend/src/app/page.tsx
@@ -1,14 +1,16 @@
-import Image from "next/image";
 import Subtext from "./_components/atom/Subtext";
 import Priority from "./_components/atom/Priority";
 import Marker from "./_components/atom/Marker";
 import Title from "./_components/atom/Title";
 import TaskItem from "./_components/molecule/TaskItem";
 import dayjs from "dayjs";
+import LimitWarningIcon from "./_components/atom/LimitWarningIcon";
+import LimitOverIcon from "./_components/atom/LimitOverIcon";
+import LimitInfoIcon from "./_components/atom/LimitInfoIcon";
 
 export default function Home() {
   return (
-    <main className="">
+    <main>
       <div className="flex items-center gap-1">
         <Subtext>sample</Subtext>
         {[...Array(8)].map((_, i) => {
@@ -18,6 +20,11 @@ export default function Home() {
         })}
         <Title>sample</Title>
         <Marker />
+      </div>
+      <div className="flex gap-1">
+        <LimitInfoIcon />
+        <LimitWarningIcon />
+        <LimitOverIcon />
       </div>
       <div className="pl-1 w-96">
         {[...Array(8)].map((_, i) => {
