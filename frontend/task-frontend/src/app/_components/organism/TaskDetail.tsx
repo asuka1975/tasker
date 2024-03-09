af3b4db787ddb1eda85f831075bed44bd2d160d5
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2024-03-10 00:20:41 +0900

    feat: 子タスク追加ボタンを追加

--- frontend/task-frontend/src/app/_components/organism/TaskDetail.tsx
+++ frontend/task-frontend/src/app/_components/organism/TaskDetail.tsx
@@ -9,6 +9,8 @@ import Subtext from "../atom/Subtext";
 import DateTimeInput from "../atom/DateTimeInput";
 import DateTimeView from "../atom/DateTimeView";
 import dayjs from "dayjs";
+import AddIconButton from "../atom/AddIconButton";
+import Button from "../atom/Button";
 
 type Props = {
     id: number
@@ -22,7 +24,7 @@ export default async function TaskDetail({ id }: Props) {
         <div className="flex flex-col w-full h-full">
             <div className="mb-8"><EditableTitle onSave={() => {}} title={task.title} /></div>
             <div className="grow grid grid-cols-[2fr_1fr] gap-2">
-                <div className=" grid grid-rows-[1fr_1fr]">
+                <div className=" grid grid-rows-[1fr_1fr_3rem]">
                     <div className="flex flex-col">
                         <Heading2>説明</Heading2>
                         <div className="grow">
@@ -30,13 +32,18 @@ export default async function TaskDetail({ id }: Props) {
                         </div>
                     </div>
                     <div>
-                        <div className="grid grid-cols-[1fr_2rem]">
+                        <div className="grid grid-cols-[1fr_3rem]">
                             <Heading2>子タスク一覧</Heading2>
-                            
+                            <div>
+                                <AddIconButton className="p-2" />
+                            </div>
                         </div>
                         <div className="grid divide-y px-2 shadow-md rounded-md shadow-gray-200 divide-gray-200">
                             {subtasks.map(task => <div key={task.id} className="py-2"><TaskItem { ...task } /></div>)}
                         </div>
+                    </div>
+                    <div>
+                        <Button>完了</Button>
                     </div>
                 </div>
                 <div className="flex flex-col gap-2 border-gray-300 border-[1px] rounded-md p-4">
