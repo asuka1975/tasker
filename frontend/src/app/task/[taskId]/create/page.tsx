268690fb819d1b4b14af7db4f3343b9d3cfa8cc5
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2024-03-24 21:23:20 +0900

    feat:　モーダルの並列ページを作成

--- frontend/src/app/task/[taskId]/create/page.tsx
+++ frontend/src/app/task/[taskId]/create/page.tsx
@@ -1,7 +1,26 @@
-type Props = {
+import BackLinkButton from "@/app/_components/molecule/BackLinkButton"
+import Header from "@/app/_components/organism/Header"
+import SubtaskForm from "@/app/_components/organism/SubtaskForm"
 
+type Props = {
+    params: {
+        taskId: number
+    }
 }
 
-export default function Page({}: Props) {
-    return <>a</>
+export default function Page({ params }: Props) {
+    return (
+        <div className="flex flex-col w-screen h-screen">
+            <Header>
+                <div className="inline-block">
+                    <BackLinkButton className="col-start-1" href="/">
+                        ホーム
+                    </BackLinkButton>
+                </div>
+            </Header>
+            <div className="grow py-4 px-[20%]">
+                <SubtaskForm parentId={params.taskId} />
+            </div>
+        </div>
+    )
 }
